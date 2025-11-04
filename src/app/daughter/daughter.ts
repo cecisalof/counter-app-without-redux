a1a521a05b7b3060bab14ebb858569c5176a5ef1
Cecilia Losada <cecilia.sanchezlosada@gmail.com>
2025-11-04 11:53:46 +0100

    [Feature]: event Emitters

--- src/app/daughter/daughter.ts
+++ src/app/daughter/daughter.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { GrandDaughter } from "../grand-daughter/grand-daughter";
 
 @Component({
   selector: 'app-daughter',
-  imports: [],
+  imports: [GrandDaughter],
+  standalone: true,
   templateUrl: './daughter.html',
   styleUrl: './daughter.scss',
 })
-export class Daughter {
+export class Daughter implements OnInit{
+  @Input() counter!: number;
+  @Output() changeCounter = new EventEmitter<number>();
 
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+  }
+  
+  duplicate(): void {
+    this.counter =this.counter * 2;
+    this.changeCounter.emit(this.counter)
+  }
+
+  resetGrandDaughter(newCounter: number): void {
+    this.counter = newCounter;
+    this.changeCounter.emit(this.counter)
+  }
 }
--- src/app/grand-daughter/grand-daughter.ts
+++ src/app/grand-daughter/grand-daughter.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-grand-daughter',
   imports: [],
+  standalone: true,
   templateUrl: './grand-daughter.html',
   styleUrl: './grand-daughter.scss',
 })
 export class GrandDaughter {
+  @Input() counter!: number;
+  @Output() changeCounter = new EventEmitter<number>();
 
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  reset(): void{
+    this.counter = 0;
+    this.changeCounter.emit(this.counter) 
+  }
 }
