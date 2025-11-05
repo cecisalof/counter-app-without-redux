22951569c8a2f67f0d5cf21808f5dd5e9ab9d474
Cecilia Losada <cecilia.sanchezlosada@gmail.com>
2025-11-05 12:25:57 +0100

    [Refactor]: refactor reducer with createReducer method

--- src/app/Counter/counter.reducer.ts
+++ src/app/Counter/counter.reducer.ts
@@ -1,17 +1,28 @@
-import { Action } from "@ngrx/store";
+import { Action, createReducer, on } from "@ngrx/store";
 import { increment, decrement } from "./counter.actions";
 
-export function counterReducer(state: number = 10, action: Action) {
-    switch (action.type) {
-        case increment.type:
-            return state + 1;
-            break;
-        
-        case decrement.type:
-            return state - 1;
-            break;
-    
-        default:
-            return state;
-    }
+// export function counterReducer(state: number = 10, action: Action) {
+//     switch (action.type) {
+//         case increment.type:
+//             return state + 1;
+//             break;
+
+//         case decrement.type:
+//             return state - 1;
+//             break;
+
+//         default:
+//             return state;
+//     }
+// }
+
+export const initialState = 10;
+
+const _counterReducer = createReducer(
+    initialState,
+    on(increment, (state) => state + 1),
+    on(decrement, (state) => state - 1)
+);
+export function counterReducer(state: number | undefined, action: Action) {
+    return _counterReducer(state, action);
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,7 +10,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideStore({
-      counter: counterReducer
+      counter: counterReducer //Se importa de counter.reducer
     })
   ]
 };
