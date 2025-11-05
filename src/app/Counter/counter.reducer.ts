4329403d0df233970f5dc529205535789d426a30
Cecilia Losada <cecilia.sanchezlosada@gmail.com>
2025-11-05 12:29:24 +0100

    [Refactor]: refactor reducer in a modern usage

--- src/app/Counter/counter.reducer.ts
+++ src/app/Counter/counter.reducer.ts
@@ -1,28 +1,10 @@
-import { Action, createReducer, on } from "@ngrx/store";
+import { createReducer, on } from "@ngrx/store";
 import { increment, decrement } from "./counter.actions";
 
-// export function counterReducer(state: number = 10, action: Action) {
-//     switch (action.type) {
-//         case increment.type:
-//             return state + 1;
-//             break;
+export const initialState = 100;
 
-//         case decrement.type:
-//             return state - 1;
-//             break;
-
-//         default:
-//             return state;
-//     }
-// }
-
-export const initialState = 10;
-
-const _counterReducer = createReducer(
+export const counterReducer = createReducer(
     initialState,
     on(increment, (state) => state + 1),
     on(decrement, (state) => state - 1)
 );
-export function counterReducer(state: number | undefined, action: Action) {
-    return _counterReducer(state, action);
-}
