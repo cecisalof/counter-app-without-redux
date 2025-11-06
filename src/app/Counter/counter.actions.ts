f4dac17a4bb66f36a48af232d726a38d20c227ce
Cecilia Losada <cecilia.sanchezlosada@gmail.com>
2025-11-06 12:30:20 +0100

    [Redux]: duplicate and reset actions & reducer were created

--- src/app/Counter/counter.actions.ts
+++ src/app/Counter/counter.actions.ts
@@ -1,5 +1,17 @@
-import { createAction } from "@ngrx/store";
+import { createAction, props } from "@ngrx/store";
 
 // actions describe what should happen: decrement, increment, etc.
 export const increment = createAction('[Counter Componente] Increment');
 export const decrement = createAction('[Counter Componente] Decrement');
+// 1. Acción Duplicar (sin payload)
+export const duplicate = createAction(
+  '[Counter Component] Duplicate',
+  props<{ number: number }>() 
+);
+
+// 2. Acción Resetear (con payload)
+// Usamos 'props' para definir el tipo de datos que llevará esta acción (el nuevo valor)
+export const reset = createAction(
+  '[Counter Component] Reset',
+  props<{ newCounter: number }>() 
+);
--- src/app/Counter/counter.reducer.ts
+++ src/app/Counter/counter.reducer.ts
@@ -1,10 +1,18 @@
 import { createReducer, on } from "@ngrx/store";
-import { increment, decrement } from "./counter.actions";
+import { increment, decrement, duplicate, reset } from "./counter.actions";
 
-export const initialState = 100;
+export const initialState = 10;
 
 export const counterReducer = createReducer(
     initialState,
     on(increment, (state) => state + 1),
-    on(decrement, (state) => state - 1)
+    on(decrement, (state) => state - 1),
+    // 1. Manejo de la acción DUPLICATE  
+    on(duplicate, (state, {number}) => state * number),
+    // 2. Manejo de la acción RESET
+    on(reset, (state, action) => {
+        // La lógica: Establecer el estado al valor del payload
+        // Usamos 'action' para acceder al payload { newCounter }
+        return action.newCounter;
+    }),
 );
