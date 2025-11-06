49befb389491d3e669c1156d35940ca928c92911
Cecilia Losada <cecilia.sanchezlosada@gmail.com>
2025-11-06 12:31:06 +0100

    [Refactor]: grand-daugther with store values

--- src/app/Counter/grand-daughter/grand-daughter.ts
+++ src/app/Counter/grand-daughter/grand-daughter.ts
@@ -1,26 +1,37 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AppState } from '../../app.reducer';
+import { Store } from '@ngrx/store';
+import { selectCounter } from '../counter.selectors';
+import * as actions from '../counter.actions'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-grand-daughter',
-  imports: [],
+  imports: [CommonModule],
   standalone: true,
   templateUrl: './grand-daughter.html',
   styleUrl: './grand-daughter.scss',
 })
-export class GrandDaughter {
-  @Input() counter!: number;
-  @Output() changeCounter = new EventEmitter<number>();
+export class GrandDaughter implements OnInit {
+  // @Input() counter!: number;
+  // @Output() changeCounter = new EventEmitter<number>();
+  //Usamos un Observable tipado para la reactividad
+  counter$!: Observable<number>;
 
 
-  constructor(){
+  constructor(private store: Store<AppState>) {
 
   }
 
   ngOnInit(): void {
+    this.counter$ = this.store.select(selectCounter)
   }
 
-  reset(): void{
-    this.counter = 0;
-    this.changeCounter.emit(this.counter) 
+  // MANEJA EL EVENTO resetCounter de daugther
+  handleReset(newCounterValue: number): void {
+    // Despachar la acción 'reset', enviando el payload
+    this.store.dispatch(actions.reset({ newCounter: newCounterValue }));
   }
+
 }
